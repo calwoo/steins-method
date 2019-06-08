4970dfe2981b9e8a6313ba918a77df655bd86fee
Calvin D. Woo <calvin.d.woo@gmail.com>
2019-06-07 20:12:11 -0400

    KSD working somewhat

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.autograd import grad
 
 from kernels import *
 from stein import *
@@ -17,18 +18,6 @@ def p(x):
     """
     return torch.exp(torch.distributions.Normal(0,1).log_prob(x))
 
-### langevin-stein operator
-def langevin_stein_g(p, phi, x):
-    """
-    implementation of the Langevin-Stein operator, which is an operator
-    variational objective useful for SVGD.
-    """
-    def logp(x):
-        return torch.log(p(x))
-    nabla_logp = grad(logp(x), x, create_graph=True)[0]
-    return torch.dot(phi(x), nabla_logp) + grad(phi(x), x, create_graph=True)[0]
-   
-"""
 num_samples=10
 
 q = torch.distributions.Normal(0,1)
@@ -36,44 +25,4 @@ samples = q.sample((num_samples,))
 
 rbf_kernel = RBFKernel(sigma=1.0)
 ksd = KSD(rbf_kernel, p)
-# ksd.eval(q, num_samples=10)
-
-phi = ksd.optimal_fn(q, samples)
-
-def kern_curry(y):
-    def k(x):
-        return ksd.kernel.eval(x,y).view(-1)
-    return k
-
-### test
-x = torch.tensor([2.0], requires_grad=True)
-y = langevin_stein(ksd.p, kern_curry(torch.tensor([1.0])), x.view(-1))
-z = langevin_stein_g(ksd.p, kern_curry(torch.tensor([1.0])), x.view(-1))
-
-y.backward()
-z.backward()
-print(y, x.grad)
-print(z, x.grad)
-"""
-
-x = torch.tensor([2.0], requires_grad=True)
-
-def f(x):
-    log_prob = torch.log(p(x))
-    log_prob.backward()
-    print(x.grad)
-    return log_prob
-
-#y = f(x)
-#print(y)
-#y.backward()
-#print(x.grad)
-
-x_clone = x.clone()
-
-print(x, x_clone)
-
-y = x**2
-y.backward()
-
-print(x.grad, x_clone.grad)
+print(ksd.eval(q, num_samples=10))
--- stein.py
+++ stein.py
@@ -8,6 +8,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.autograd import grad
 
 
 ### langevin-stein operator
@@ -16,19 +17,11 @@ def langevin_stein(p, phi, x):
     implementation of the Langevin-Stein operator, which is an operator
     variational objective useful for SVGD.
     """
-    # attractive term
-    x_a = x.detach().requires_grad_()
-    log_prob = torch.log(p(x_a))
-    log_prob.backward()
-    a_term = torch.dot(phi(x_a).view(-1), x_a.grad)
-    
-    # repulsive term
-    x_r = x.detach().requires_grad_()
-    phi_x = phi(x_r).view(-1)
-    phi_x.backward(torch.ones_like(x_r))
-    r_term = torch.sum(x_r.grad)
-    
-    return a_term + r_term
+    def logp(y):
+        return torch.log(p(y))
+    nabla_logp = grad(logp(x), x, create_graph=True)[0]
+    phi_x = phi(x).view(-1)
+    return torch.dot(phi_x, nabla_logp) + grad(phi_x, x, create_graph=True)[0]
 
 ### Kernelized Stein discrepancy
 class KSD:
@@ -51,11 +44,9 @@ class KSD:
         def phi_star(x):
             num_samples = samples.shape[0]
             phi_vals = torch.zeros(samples.shape)
-            print("poop")
             for i in range(num_samples):
-                sp = samples[i]
+                sp = samples[i].clone().detach().requires_grad_()
                 phi_vals[i] = langevin_stein(self.p, kern_curry(sp), x.view(-1))
-            print("jib")
             return torch.mean(phi_vals)
     
         return phi_star
@@ -70,7 +61,6 @@ class KSD:
         new_samples = q.sample((num_samples,))
         ksd_vals = torch.zeros(samples.shape)
         for i in tqdm(range(num_samples)):
-            sp = new_samples[i]
+            sp = samples[i].clone().detach().requires_grad_()
             ksd_vals[i] = langevin_stein(self.p, phi_star, sp.view(-1))
-            print("vroom!")
         return torch.mean(ksd_vals)
